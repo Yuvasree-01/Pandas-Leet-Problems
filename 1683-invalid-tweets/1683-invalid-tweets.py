3daf60f88b29c038c60d051f1332893cc08f7836
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2026-07-10 23:06:20 +0530

    Time: 286 ms (67.98%), Space: 67.6 MB (15.39%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.py
+++ 1683-invalid-tweets/1683-invalid-tweets.py
@@ -4,5 +4,8 @@ def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     # result = tweets["content"].str.len()>15
     # result = tweets[result][["tweet_id"]]
     # return result
+
     mask = [len(text) > 15 for text in tweets["content"]]
     return tweets.loc[mask, ["tweet_id"]]
+
+    # return tweets.loc[tweets["content"].str.len() > 15, ["tweet_id"]]
