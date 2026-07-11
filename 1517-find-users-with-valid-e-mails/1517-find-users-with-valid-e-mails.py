2f4dbf6f9399d7aafe4abfb985a908cc9c2aa181
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2026-07-11 23:50:35 +0530

    Time: 273 ms (96.17%), Space: 67.1 MB (96.66%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
@@ -1,6 +1,31 @@
 import pandas as pd
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
-    pattern = r'^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$'
+    # pattern = r'^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$'
     
-    return users[users["mail"].str.match(pattern)]
+    # return users[users["mail"].str.match(pattern)]
+
+    def is_valid(email):
+        
+        if not email.endswith("@leetcode.com"):
+            return False
+
+        username = email[:-13]
+
+        if not username:
+            return False
+
+        if not username[0].isalpha():
+            return False
+
+        for ch in username:
+            if not (
+                ch.isalnum()
+                or ch in "_.-"
+            ):
+                return False
+
+        return True
+
+
+    return users[users["mail"].apply(is_valid)]
