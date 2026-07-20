90ce6bd93e9bdc41d2b0cfb1418cdb9008e04b56
Yuvasree-01 <121015977+Yuvasree-01@users.noreply.github.com>
2026-07-20 23:05:51 +0530

    Delete 1050-actors-and-directors-who-cooperated-at-least-three-times directory

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
-    counts= actor_director.groupby(['actor_id','director_id'],as_index=False).size()
-    result=counts[counts['size']>=3]
-    return result[['actor_id','director_id']]
